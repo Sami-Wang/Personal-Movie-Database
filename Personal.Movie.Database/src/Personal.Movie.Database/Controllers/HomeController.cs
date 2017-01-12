d7f995f23da1a54a9f050bbf98981d50e571ec83
Yihang Wang <wyhliverpoolfc@gmail.com>
2017-01-12 14:09:24 +1100

    feat: implement the backend logic of search movie by title function Implement the backend logic of search movie by title function.

--- Personal.Movie.Database/src/Personal.Movie.Database/Controllers/HomeController.cs
+++ Personal.Movie.Database/src/Personal.Movie.Database/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Personal.Movie.Database.Model.MovieModel;
+using Personal.Movie.Database.TMDbAPI;
 
 namespace Personal.Movie.Database.Controllers
 {
@@ -16,6 +18,12 @@ namespace Personal.Movie.Database.Controllers
         public async Task<IActionResult> MovieDetailsInfo(int? id)
         {
             return View(id);
+        }
+
+        public async Task<IActionResult> SearchMovies(string movieTitle)
+        {
+            List<MovieBriefInfo> searchMovies = await MovieEnquiry.SearchMovieByTitle(movieTitle);
+            return View(searchMovies);
         }
     }
 }
